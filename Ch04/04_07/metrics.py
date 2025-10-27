1d936be0571ccdc218afcd7ad1c26f04b445fa0b
Shiv Katira <shivkatira@gmail.com>
2025-10-27 06:14:00 +0000

    Refactor metrics.py to improve schema validation and code consistency

--- Ch04/04_07/metrics.py
+++ Ch04/04_07/metrics.py
@@ -1,20 +1,40 @@
 import pandas as pd
+import pandera.pandas as pa
+from pandera import check_output
 
 
+def is_cpu_or_mem(text):
+    cpu_mem = ["cpu", "mem"]
+    if text in cpu_mem:
+        return True
+    return False
+
+
+metrics_schema = pa.DataFrameSchema(
+    {
+        "time": pa.Column(pd.DatetimeTZDtype("ns", "UTC")),
+        "metric": pa.Column(str, checks=pa.Check(is_cpu_or_mem, element_wise=True)),
+        "value": pa.Column(pd.Float64Dtype, checks=pa.Check.greater_than(0)),
+    }
+)
+
+
+@check_output(metrics_schema)
 def load_metrics(jsonl_file):
     return pd.read_json(
         jsonl_file,
-        orient='records',
+        orient="records",
         lines=True,
-        convert_dates=['time'],
+        convert_dates=["time"],
     )
 
 
 # testing
-if __name__ == '__main__':
+if "__name__" == "__main__":
     from pathlib import Path
 
     here = Path(__file__).absolute().parent
-    jsonl_file = here / 'metrics.jsonl'
+    jsonl_file = here / "metrics.jsonl"
 
-    load_metrics(jsonl_file)
+    df = load_metrics(jsonl_file)
+    metrics_schema.validate(df)
